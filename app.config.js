d9c1c8d29244ac7204b79c05be83f47961c16073
Timothy Russo <russotimothy@live.it>
2025-04-23 08:46:42 +0200

    fix: update build workflow

--- app.config.js
+++ app.config.js
@@ -1,8 +1,15 @@
-export default {
-  android: {
-    googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './google-services.json',
-  },
-  ios: {
-    googleServicesFile: process.env.GOOGLE_SERVICES_PLIST ?? './GoogleService-Info.plist',
-  },
+export default ({ expo }) => {
+  return {
+    ...expo,
+    ios: {
+      config: {
+        googleServicesFile: process.env.GOOGLE_SERVICES_PLIST ?? './GoogleService-Info.plist',
+      },
+    },
+    android: {
+      config: {
+        googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './google-services.json',
+      },
+    },
+  };
 };
